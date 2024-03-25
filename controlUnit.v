a855b6c128807d3904ee87d1b173d944a84ff8fa
PradhyumnaVA <137704414+PradhyumnaVA@users.noreply.github.com>
2024-03-25 19:01:35 +0530

    Add files via upload
    gg

--- controlUnit.v
+++ controlUnit.v
@@ -2,28 +2,47 @@ module controlUnit (
     input [2:0] funct3,
     input [6:0] funct7,
     input [6:0] op,
-    output reg [3:0] ALUcontrol
+    output reg [3:0] ALUcontrol,
+    output reg [1:0] ImmSrc,
+    output reg reg_write
 );
+
+localparam [6:0]R_TYPE  = 7'b0110011,
+                I_TYPE  = 7'b0010011,
+                STORE   = 7'b0100011,
+                LOAD    = 7'b0000011,
+                BRANCH  = 7'b1100011,
+                JALR    = 7'b1100111,
+                JAL     = 7'b1101111,
+                AUIPC   = 7'b0010111,
+                LUI     = 7'b0110111;
 
 always @(funct3,funct7,op) begin
     case (op)
         //R-Type instruction
-        7'b0110011:begin
+        R_TYPE :begin
+            reg_write <= 1;
             case (funct3)
-                3'd0: begin
-                    if (funct7==7'd32) begin
-                        ALUcontrol = 4'b0001;
+                0: begin
+                    if (funct7 == 7'd32) begin
+                        ALUcontrol <= 4'b0001;
                     end
-                    else ALUcontrol = 4'b0000;
+                    else ALUcontrol <= 4'b0000;
                 end
-                3'd7: ALUcontrol = 4'b0010;
-                3'd6: ALUcontrol = 4'b0011;
-                3'd1: ALUcontrol = 4'b0100;
-                3'd5: ALUcontrol = 4'b0101;
-                3'd4: ALUcontrol = 4'b0111;
-                3'd2: ALUcontrol = 4'b1000; 
+                7: ALUcontrol <= 4'b0010;
+                6: ALUcontrol <= 4'b0011;
+                1: ALUcontrol <= 4'b0100;
+                5: ALUcontrol <= 4'b0101;
+                4: ALUcontrol <= 4'b0111;
+                2: ALUcontrol <= 4'b1000; 
             endcase
-        end 
+        end
+
+        LOAD :begin
+            reg_write<=1;
+            ALUcontrol<=4'b0000;
+            ImmSrc<=2'b00;
+        end
     endcase
 end
 endmodule
